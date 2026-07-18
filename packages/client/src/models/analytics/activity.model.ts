3c3e41d0b620585f963907550922f8f1bb178d7b
VincentShipsIt <vincent@shipshit.dev>
2026-07-18 19:06:58 +0200

    fix(client): expose activity action origin

--- packages/client/src/models/analytics/activity.model.ts
+++ packages/client/src/models/analytics/activity.model.ts
@@ -1,5 +1,5 @@
 import { BaseEntity } from '@genfeedai/client/models/base/base-entity.model';
-import type { ActivityStatus } from '@genfeedai/enums';
+import type { ActionOrigin, ActivityStatus } from '@genfeedai/enums';
 import type { IActivity, IOrganization, IUser } from '@genfeedai/interfaces';
 
 export class Activity extends BaseEntity implements IActivity {
@@ -9,6 +9,7 @@ export class Activity extends BaseEntity implements IActivity {
   public declare value: string;
   public declare status?: ActivityStatus;
   public declare source: string;
+  public declare origin: ActionOrigin;
   public declare isRead: boolean;
 
   constructor(data: Partial<IActivity> = {}) {
