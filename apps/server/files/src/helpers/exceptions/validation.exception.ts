2dcfa13d373b1a6b80809f799938c9195280ba26
VincentShipsIt <vincent@shipshit.dev>
2026-07-19 07:06:42 +0200

    chore(backend): remove dead utility surfaces (#1889)

--- apps/server/files/src/helpers/exceptions/validation.exception.ts
+++ apps/server/files/src/helpers/exceptions/validation.exception.ts
@@ -4,17 +4,3 @@ export class ValidationException extends Error {
     this.name = 'ValidationException';
   }
 }
-
-export class FileValidationException extends ValidationException {
-  constructor(message: string) {
-    super(message);
-    this.name = 'FileValidationException';
-  }
-}
-
-export class PathValidationException extends ValidationException {
-  constructor(message: string) {
-    super(message);
-    this.name = 'PathValidationException';
-  }
-}
