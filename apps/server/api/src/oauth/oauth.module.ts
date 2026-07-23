ed1cf190128fe361bcfe6fcee7af5868166b7ffe
VincentShipsIt <vincent@shipshit.dev>
2026-07-24 00:44:49 +0200

    refactor(api): avoid OAuth module forward reference

--- apps/server/api/src/oauth/oauth.module.ts
+++ apps/server/api/src/oauth/oauth.module.ts
@@ -1,6 +1,6 @@
 import { BetterAuthModule } from '@api/auth/better-auth/better-auth.module';
 import { ApiKeysModule } from '@api/collections/api-keys/api-keys.module';
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { OAuthAuthorizeController } from './controllers/oauth-authorize.controller';
 import { OAuthRegisterController } from './controllers/oauth-register.controller';
 import { OAuthTokenController } from './controllers/oauth-token.controller';
@@ -13,7 +13,7 @@ import { OAuthClientService } from './services/oauth-client.service';
     OAuthAuthorizeController,
     OAuthTokenController,
   ],
-  imports: [BetterAuthModule, forwardRef(() => ApiKeysModule)],
+  imports: [BetterAuthModule, ApiKeysModule],
   providers: [OAuthClientService, OAuthAuthorizeService],
 })
 export class OAuthModule {}
