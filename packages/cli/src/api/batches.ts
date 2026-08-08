9f4043cf797b175f7d2d1cc7af8199337bd274e9
VincentShipsIt <vincent@shipshit.dev>
2026-08-08 16:54:44 +0200

    fix(cli): send brandId when creating batches

--- packages/cli/src/api/batches.ts
+++ packages/cli/src/api/batches.ts
@@ -44,7 +44,7 @@ export interface BatchDetail extends Batch {
 export interface CreateBatchRequest {
   count: number;
   platforms: string[];
-  brand: string;
+  brandId: string;
   topics?: string[];
   style?: string;
   startDate?: string;
